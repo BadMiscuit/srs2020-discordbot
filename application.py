cef585cd6aae8367d82c6bb1c09d98ec89248322
badmiscuit <badmiscuit@freep0m>
2020-06-02 20:51:47 +0200

    Fixed play and leave

--- application.py
+++ application.py
@@ -5,8 +5,18 @@ import asyncio
 from datetime import datetime
 from gtts import gTTS
 import random
+import os
 
 client = discord.Client()
+
+async def play(voice, filename):
+    voice.play(discord.FFmpegPCMAudio(filename))
+    while (True):
+        if not voice.is_playing():
+            await voice.disconnect()
+            break
+    os.remove(filename)
+    return
 
 @client.event
 async def on_ready():
@@ -16,20 +26,16 @@ async def on_ready():
 async def on_voice_state_update(member, before, after):
     if (member.id != 675345725512613899):
         if (after.channel != None and before.channel != after.channel):
+            msg = "{0} a rejoint le canal".format(member.name)
+            sound = gTTS(text=msg, lang='fr', slow=False)
             print("{0}: {1} joined {2}".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), member.name, after.channel.name))
             try:
-                voice_client = await after.channel.connect()
-                msg = "{0} a rejoint le canal".format(member.name)
-                sound = gTTS(text=msg, lang='fr', slow=False)
+                voice = await after.channel.connect()
                 filename = "voice_{0}.mp3".format(random.randint(0, 1024))
                 sound.save(filename)
-                try:
-                    voice_client.play(discord.FFmpegPCMAudio(filename))
-                    print("Ok")
-                except Exception as e:
-                    print(str(e))
-            finally:
-                await voice_client.disconnect()
+                await play(voice, filename)
+            except Exception as e:
+                print("Error: " + str(e))
         elif (before.channel != None and after.channel != before.channel):
             print("{0}: {1} left {2}".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), member.name, before.channel.name))
     return
