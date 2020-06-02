4d1161cc045211c9c8f11058cc139d24fd02ff0e
Ubuntu <user01@srs-discordbot.3y2eo3aehzberdy5g5iuzwjw5a.ax.internal.cloudapp.net>
2020-06-02 20:52:06 +0000

    Minor fixes

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 import discord
 from config import *
-import time
 import asyncio
 from datetime import datetime
 from gtts import gTTS
@@ -18,35 +17,43 @@ async def play(voice, filename):
     os.remove(filename)
     return
 
+async def leave():
+    for e in client.voice_clients:
+        try:
+            if (e.is_playing()):
+                e.stop()
+            await e.disconnect()
+            break
+        except:
+            continue
+
+def tts(name):
+    msg = "{0} a rejoint le canal".format(name)
+    sound = gTTS(text=msg, lang='fr', slow=False)
+    filename = "voice_{0}.mp3".format(random.randint(0, 16))
+    sound.save(filename)
+    return filename
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user.name}:{0.user.id}'.format(client))
 
 @client.event
 async def on_voice_state_update(member, before, after):
-    if (member.id != 675345725512613899):
+    if (member.id != CLIENT_ID):
         if (after.channel != None and before.channel != after.channel):
-            msg = "{0} a rejoint le canal".format(member.name)
-            sound = gTTS(text=msg, lang='fr', slow=False)
-            print("{0}: {1} joined {2}".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), member.name, after.channel.name))
+            print("{0}: {1} joined {2}".format(
+                datetime.now().strftime("%d/%m/%Y %H:%M:%S"), 
+                member.display_name, after.channel.name))
+            await leave()
+            filename = tts(member.display_name)
             try:
                 voice = await after.channel.connect()
-                filename = "voice_{0}.mp3".format(random.randint(0, 1024))
-                sound.save(filename)
                 await play(voice, filename)
-            except Exception as e:
-                print("Error: " + str(e))
+            except discord.ClientException:
+                await leave()
         elif (before.channel != None and after.channel != before.channel):
             print("{0}: {1} left {2}".format(datetime.now().strftime("%d/%m/%Y %H:%M:%S"), member.name, before.channel.name))
     return
 
-'''
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-'''
 client.run(TOKEN)
