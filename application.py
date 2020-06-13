50606833e30b886632e5906bc2098db50d5b2c6f
Ubuntu <user01@srs-discordbot.3y2eo3aehzberdy5g5iuzwjw5a.ax.internal.cloudapp.net>
2020-06-13 11:59:51 +0000

    Removed files deletion, added delay before playing and leaving, fixed voice leaving algorithm and added logs on standard output

--- application.py
+++ application.py
@@ -1,11 +1,10 @@
 import discord
 from config import *
 import asyncio
-from datetime import datetime
 from gtts import gTTS
-import random
 import os
-import glob
+import time
+from datetime import datetime
 
 client = discord.Client()
 
@@ -15,62 +14,52 @@ async def on_ready():
     return
 
 async def play(voice, filename):
+    time.sleep(0.5)
+    print("[{0}]: Playing {1}".format(
+        datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+        filename))
     voice.play(discord.FFmpegPCMAudio(filename))
-    while (True):
-        if not voice.is_playing():
-            remove(filename)
-            await leave(voice)
-            break
-    return
-
-def remove(filename=None):
-    if (filename == None):
-        fileList = glob.glob('voice_*.mp3')
-        for f in fileList:
-            try:
-                os.remove(f)
-            except:
-                continue
-    else:
-        try:
-            os.remove(filename)
-        except:
-            pass
+    while (voice.is_playing()):
+        time.sleep(0.5)
+    print("[{0}]: Played {1}".format(
+        datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+        filename))
     return
 
 async def leave(voice=None):
-    if (voice == None):
-        for c in client.voice_clients:
-            if (c.is_playing()):
-                c.stop()
-            if (c.is_connected()):
-                await e.disconnect()
-                break
-    else:
-        if (voice.is_playing()):
-            voice.stop()
-        await voice.disconnect(force=True)
+    if (len(client.voice_clients) < 1):
+        return
+    if (voice == None and len(client.voice_clients) > 0):
+        voice = client.voice_clients[0]
+    if (voice.is_playing()):
+        voice.stop()
+    await voice.disconnect(force=True)
     return
 
-
 def tts(name):
-    msg = "{0} a rejoint le canal".format(name)
+    filename = "files/voice_{0}.mp3".format(name)
+    if os.path.exists(filename):
+        return filename
+    msg = "Bonjour {0}".format(name)
     sound = gTTS(text=msg, lang='fr', slow=False)
-    filename = "voice_{0}.mp3".format(random.randint(0, 16))
     sound.save(filename)
     return filename
+
 
 @client.event
 async def on_voice_state_update(member, before, after):
     if (not member.bot and member.id != CLIENT_ID):
         if (after.channel != None and before.channel != after.channel):
             await leave()
-            remove()
             filename = tts(member.display_name)
             try:
+                print("Joining {0}".format(after.channel))
                 voice = await after.channel.connect()
+                print("Joined {0}".format(after.channel))
                 await play(voice, filename)
+                await leave(voice)
             except discord.ClientException:
+                print("Already in voice")
                 await leave()
     return
 
