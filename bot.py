23b343a94d39b30c965f2103b502072b5b34cf3d
BadMiscuit <tr.nguyenanh@gmail.com>
2020-06-23 21:23:43 +0000

    Changed hardcoded variables for global constants; Moved announcer to main; Added a renamer feature

--- bot.py
+++ bot.py
@@ -3,9 +3,10 @@
 from announcer import *
 from poll import send_poll, poll_add_option
 from config import *
-from announcer import announce
+from announcer import *
 import discord
 from discord.ext import commands
+from discord.ext.commands import has_permissions
 
 bot = commands.Bot(command_prefix='/')
 
@@ -15,7 +16,26 @@ async def on_ready():
 
 @bot.event
 async def on_voice_state_update(member, before, after):
-    await announce(bot.voice_clients, member, before, after)
+    if (not member.bot and member.id != CLIENT_ID):
+        if (after.channel != None and before.channel != after.channel):
+            await leave(bot.voice_clients)
+            filename = tts(member.display_name)
+            try:
+                voice = await after.channel.connect()
+                print("Joined {0}".format(after.channel))
+                await play(voice, filename)
+                await leave(bot.voice_clients, voice)
+            except discord.ClientException:
+                print("Error trying to join {0}: Already in voice".format(after.channel))
+                await leave(bot.voice_clients)
+            except Exception as e:
+                print(str(e))
+                await leave(bot.voice_clients)
+
+@bot.command()
+async def test(ctx, *args):
+    for r in ctx.guild.roles:
+        print("{0} {1}".format(r.id, r.name))
 
 @bot.command()
 async def poll_all(ctx, *args):
@@ -43,5 +63,27 @@ async def poll(ctx, *args):
     except Exception as e:
         print(str(e))
         await ctx.message.add_reaction(ctx.guild.get_emoji("pepeangry:702518264332550156"))
+
+@bot.listen()
+async def on_message(message):
+    for role in message.author.roles:
+        if (role.id == NULL_ID):
+            return
+    if (message.channel.id == WRONG_CHAN):
+        if (message.content.startswith("-play")
+                or message.content.startswith(">>play")
+                or message.content.startswith("--play")):
+            try:
+                await message.channel.send("""Mauvais canal {0} {1}""".format(bot.get_channel(RIGHT_CHAN).mention, PEPEANGRY))
+            except Exception as e:
+                print(str(e))
+    elif (message.content.startswith("Je suis ")):
+        try:
+            nickname = message.content.replace("Je suis ", "", 1)
+            await message.author.edit(nick=nickname)
+            await message.add_reaction("\N{White Heavy Check Mark}")
+        except Exception as e:
+            print("Error in Je suis : {0}".format(str(e)))
+            await message.add_reaction("\N{Cross Mark}")
 
 bot.run(TOKEN)
