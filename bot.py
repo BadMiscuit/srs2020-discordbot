562a33c7c81305df91fe1d177a9144974402e603
badmiscuit <tr.nguyenanh@gmail.com>
2020-12-06 22:46:41 +0000

    Overhaul code structure to cogs, moved emojis to a separate file

--- bot.py
+++ bot.py
@@ -1,89 +1,41 @@
 #!/usr/bin/env python3
 
-from announcer import *
-from poll import send_poll, poll_add_option
-from config import *
-from announcer import *
 import discord
+import modules.shifumi.shifumi, modules.poll.poll, modules.announcer.announcer
+
 from discord.ext import commands
 from discord.ext.commands import has_permissions
+from utils.config import NULL_ID, TOKEN, WRONG_CHANNEL, RIGHT_CHANNEL
+from utils.emojis import *
 
-
-import random
-import rstr
-
-import cogs.shifumi
 
 bot = commands.Bot(command_prefix='/')
 
+extensions = ['modules.shifumi.shifumi',
+        'modules.poll.poll',
+        'modules.announcer.announcer']
+
 if __name__ == '__main__':
-    bot.load_extension('cogs.shifumi')
+    for ext in extensions:
+        bot.load_extension(ext)
 
 @bot.event
 async def on_ready():
     print('We have logged in as {0.user.name}:{0.user.id}'.format(bot))
 
-@bot.event
-async def on_voice_state_update(member, before, after):
-    if (not member.bot and member.id != CLIENT_ID):
-        if (after.channel != None and before.channel != after.channel):
-            await leave(bot.voice_clients)
-            filename = tts(member.display_name)
-            print("File created")
-            try:
-                voice = await after.channel.connect()
-                print("Joined {0}".format(after.channel))
-                await play(voice, filename)
-                await leave(bot.voice_clients, voice)
-            except discord.ClientException:
-                print("Error trying to join {0}: Already in voice".format(after.channel))
-                await leave(bot.voice_clients)
-            except Exception as e:
-                print("Foo " + str(e))
-                await leave(bot.voice_clients)
-
-@bot.command()
-async def poll_all(ctx, *args):
-    try:
-        if (len(args) != 0):
-            await send_poll(ctx, *args, msg="@here")
-        else:
-            await send_poll(ctx, *args)
-    except Exception as e:
-        print(str(e))
-        await ctx.message.add_reaction(\
-                ctx.guild.get_emoji("pepeangry:702518264332550156"))
-
-@bot.command()
-async def poll_add(ctx, *args):
-    try:
-        await poll_add_option(ctx, *args)
-    except Exception as e:
-        print(str(e))
-        await ctx.message.add_reaction(\
-                ctx.guild.get_emoji("pepeangry:702518264332550156"))
-
-@bot.command()
-async def poll(ctx, *args):
-    try:
-        await send_poll(ctx, *args)
-    except Exception as e:
-        print(str(e))
-        await ctx.message.add_reaction(\
-                ctx.guild.get_emoji("pepeangry:702518264332550156"))
 
 @bot.listen()
 async def on_message(message):
     for role in message.author.roles:
         if (role.id == NULL_ID):
             return
-    if (message.channel.id == WRONG_CHAN):
+    if (message.channel.id == WRONG_CHANNEL):
         if (message.content.startswith("-play")
                 or message.content.startswith(">>play")
                 or message.content.startswith("--play")):
             try:
                 await message.channel.send("""Mauvais canal {0} {1}""".format(\
-                    bot.get_channel(RIGHT_CHAN).mention, PEPEANGRY))
+                    bot.get_channel(RIGHT_CHANNEL).mention, PEPEANGRY))
             except Exception as e:
                 print(str(e))
     elif (message.content.startswith("Je suis ")):
--- utils/util.py
+++ utils/util.py
@@ -1,9 +1,19 @@
 import discord
 from discord.ext import commands
 
-from config import *
+from .config import GUILD_ID, POLL_CHANNEL, TEST_CHANNEL, LOGTRACE_CHANNEL
+
 
 def srs_only():
     async def predicate(ctx):
         return ctx.guild and ctx.guild.id == GUILD_ID
     return commands.check(predicate)
+
+def poll_only():
+    async def predicate(ctx):
+        return ctx.channel.id == POLL_CHANNEL or ctx.channel.id == TEST_CHANNEL
+    return commands.check(predicate)
+
+async def logtrace(ctx, msg):
+    channel = ctx.guild.get_channel(LOGTRACE_CHANNEL)
+    await channel.send("Error: {0}".format(msg))
